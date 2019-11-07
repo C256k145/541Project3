498d4f2eea347123f2de39ab241ae16e1379b50a
c256k145 <noremackientz@gmail.com>
2019-11-06 18:24:22 -0600

    working receiver code

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,10 +1,27 @@
-int recv_pin = 8;
+int recv_pin = 2;
+int signal = 0;
+char value = 0;
 void setup() {
  Serial.begin(9600);
 }
 
 void loop() {
-  delay(100);
-  int signal = digitalRead(recv_pin);
-  Serial.println(signal);
+  delay(10);
+  signal = digitalRead(recv_pin);
+  value = value >> 1;
+  bitWrite(value, 7, signal);
+//  Serial.println(value);
+  if(value == 'k') {
+    retrieve();
+  }
+}
+
+void retrieve() {
+  for(int i = 0; i < 8; i++) {
+    delay(10);
+    signal = digitalRead(recv_pin);
+    value = value >> 1;
+    bitWrite(value, 7, signal);
+  }
+  Serial.print(value);
 }
