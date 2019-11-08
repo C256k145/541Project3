fd7615a78ac59a2db3d8668ae32c8035577f7b56
c256k145 <noremackientz@gmail.com>
2019-11-08 11:20:37 -0600

    current transmitter code

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,21 +1,33 @@
-int out_pin = 8;
-void setup() {
-  Serial.begin(19200);
+int out_pin = 3;
+void setup() 
+{
+  Serial.begin(9600);
   pinMode(out_pin, OUTPUT);
 }
 
-void loop() {
-  if(Serial.available() > 0) {
+void loop() 
+{
+  if(Serial.available() > 0) 
+  {
+    delay(10);
     char serial_data = Serial.read();
-    switch(serial_data) {
-      case 'd':
-        digitalWrite(out_pin, HIGH);
-        Serial.println("high");
-        break;
-      case 'a':
-        digitalWrite(out_pin, LOW);
-        Serial.println("low");
-        break;
+    send('k');
+    send(serial_data);
+  }
+}
+
+void send(char character)
+{
+  for(int i=0; i<8; i++)
+  {
+    delay(1);
+    if((character >> i) & 1)
+    {
+      digitalWrite(out_pin, HIGH);
+    }
+    else
+    {
+      digitalWrite(out_pin, LOW);
     }
   }
 }
