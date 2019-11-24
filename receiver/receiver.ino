f68c88540ddab75ea6d595ebb0216a05948b5674
c256k145 <noremackientz@gmail.com>
2019-11-24 17:13:29 -0600

    transmitter code

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,27 +1,23 @@
-int recv_pin = 2;
-int signal = 0;
-char value = 0;
+#include <FreqCount.h>
+
 void setup() {
- Serial.begin(9600);
+  Serial.begin(9600);
+  FreqCount.begin(1000);
 }
 
 void loop() {
-  delay(10);
-  signal = digitalRead(recv_pin);
-  value = value >> 1;
-  bitWrite(value, 7, signal);
-//  Serial.println(value);
-  if(value == 'k') {
-    retrieve();
+  if (FreqCount.available()) {
+    unsigned long count = FreqCount.read();
+    Serial.println(count);
   }
 }
 
-void retrieve() {
-  for(int i = 0; i < 8; i++) {
-    delay(10);
-    signal = digitalRead(recv_pin);
-    value = value >> 1;
-    bitWrite(value, 7, signal);
-  }
-  Serial.print(value);
-}
+//void retrieve() {
+//  for(int i = 0; i < 8; i++) {
+//    delay(10);
+//    signal = digitalRead(recv_pin);
+//    value = value >> 1;
+//    bitWrite(value, 7, signal);
+//  }
+//  Serial.print(value);
+//}
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,33 +1,30 @@
+#include <PWM.h>
 int out_pin = 3;
-void setup() 
-{
+char serial_data;
+int32_t high_freq = 93000;
+int32_t low_freq = 87000;
+
+void send(int bit) {
+  if(bit == 1) {
+    SetPinFrequency(out_pin, high_freq);
+  }
+  else {
+    SetPinFrequency(out_pin, low_freq);
+  }
+}
+
+void setup()  {
   Serial.begin(9600);
   pinMode(out_pin, OUTPUT);
 }
 
-void loop() 
-{
-  if(Serial.available() > 0) 
-  {
-    delay(10);
-    char serial_data = Serial.read();
-    send('k');
-    send(serial_data);
-  }
-}
-
-void send(char character)
-{
-  for(int i=0; i<8; i++)
-  {
-    delay(1);
-    if((character >> i) & 1)
-    {
-      digitalWrite(out_pin, HIGH);
-    }
-    else
-    {
-      digitalWrite(out_pin, LOW);
+void loop() {
+  if(Serial.available() > 0) {
+    serial_data = Serial.read();
+    for(int i = 0; i < 8; i++) {
+      int bit_to_send = serial_data && 1;
+      send(bit_to_send);
+      serial_data >> 1;
     }
   }
 }
