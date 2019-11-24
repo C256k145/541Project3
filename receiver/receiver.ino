fe8860f605d0af74f99ec292ba992be4626bb42c
c256k145 <noremackientz@gmail.com>
2019-11-24 17:39:46 -0600

    in theory, this should work. however it is completely untested

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,23 +1,39 @@
-#include <FreqCount.h>
+int input_pin = A3; //Analogread should be fast enough here
+float input_val;
+char output;
+
+/*These values specify the range of analog values we should expect to see from the input pin,
+everything in between this values is treated as null*/
+float high_freq_val; //If analogread() returns higher than this value, we read a '1'
+float low_freq_val; //If analogread() returns low than this value, we read a '0'
 
 void setup() {
   Serial.begin(9600);
-  FreqCount.begin(1000);
+  pinMode(input_pin, INPUT);
 }
 
 void loop() {
-  if (FreqCount.available()) {
-    unsigned long count = FreqCount.read();
-    Serial.println(count);
+  input_val = analogRead(input_pin);
+  if(input_val > high_freq_val || input_val < low_freq_val) {
+    retrieve(input_val); 
   }
 }
 
-//void retrieve() {
-//  for(int i = 0; i < 8; i++) {
-//    delay(10);
-//    signal = digitalRead(recv_pin);
-//    value = value >> 1;
-//    bitWrite(value, 7, signal);
-//  }
-//  Serial.print(value);
-//}
+void retrieve(float start_val) {
+  if(start_val > high_freq_val) {
+    bitWrite(output, 0, 1);
+  }
+  else {
+    bitWrite(output, 0, 0);
+  }
+  for(int i = 1; i < 8; i ++) {
+    input_val = analogRead(input_pin);
+    if(input_val > high_freq_val) {
+      bitWrite(output, i, 1);
+    }
+    else {
+      bitWrite(output, 0, 0);
+    }
+  }
+  Serial.println(output);
+}
